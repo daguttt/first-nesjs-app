905c373a05374807cda0d6efffdf1b10d060de14
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-07-21 17:44:23 -0500

    feat(): Resolve task 6 NestJS I

--- src/basic-crud/basic-crud.controller.ts
+++ src/basic-crud/basic-crud.controller.ts
@@ -6,30 +6,40 @@ import {
   Param,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
 
 @Controller('basic-crud')
 export class BasicCrudController {
   @Get('')
-  get() {
-    return { name: 'GET' };
+  get(
+    @Query('ejemplo1') example1: string,
+    @Query('ejemplo2') example2: string,
+    @Query('ejemplo3') example3: string,
+  ) {
+    return {
+      name: 'GET',
+      example1,
+      example2,
+      example3,
+    };
   }
 
   @Post('')
   post(@Body() body: any) {
     console.log(body);
-    return { name: 'POST' };
+    return { name: 'POST', receivedData: body };
   }
 
   @Put(':id')
-  put(@Param('id') id: string) {
+  put(@Param('id') id: string, @Body() body: any) {
     console.log(id);
-    return { name: 'PUT' };
+    return { name: 'PUT', routeParam: id, receivedData: body };
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
     console.log(id);
-    return { name: 'DELETE' };
+    return { name: 'DELETE', id };
   }
 }
